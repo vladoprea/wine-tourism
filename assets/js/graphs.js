b1db25f32c2ad64623e5528ee0e3b8291cb0eeb6
vladoprea <opreavladteofil@gmail.com>
2019-10-18 23:51:31 +0100

    Add PieChart for vineyard surfaces

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -9,22 +9,39 @@ function makeGraphs(error, wineProduction) {
 
     production_by_country(ndx);
 
+    vineyard_surface_by_country(ndx);
+
     dc.renderAll();
 }
 
 // Pie Chart for wine production by country
 
 function production_by_country(ndx) {
-    var name_dim = ndx.dimension(dc.pluck('Country'));
-    var wine_prod = name_dim.group().reduceSum(dc.pluck('Wine'));
+    var wine_dim = ndx.dimension(dc.pluck('Country'));
+    var wine_prod = wine_dim.group().reduceSum(dc.pluck('Wine'));
 
     dc.pieChart('#country-production')
         .height(350)
         .radius(500)
         .innerRadius(50)
         .transitionDuration(500)
-        .dimension(name_dim)
+        .dimension(wine_dim)
         .group(wine_prod);
         
+}
+
+// Pie Chart for vineyard surfaces by each country
+
+function vineyard_surface_by_country(ndx) {
+    var vineyard_dim = ndx.dimension(dc.pluck('Country'));
+    var vineyard_area = vineyard_dim.group().reduceSum(dc.pluck('Surface'));
+
+    dc.pieChart('#vineyard_surface')
+        .height(350)
+        .radius(500)
+        .innerRadius(50)
+        .transitionDuration(500)
+        .dimension(vineyard_dim)
+        .group(vineyard_area);
 }
 
