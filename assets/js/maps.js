3842f516649d57d1dbe95b6716fce555e6c70593
vladoprea <opreavladteofil@gmail.com>
2019-10-22 23:29:05 +0100

    Add function for initiaize map

---
+++ assets/js/maps.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: {
+            lat: 41.8719,
+            lng: 12.5674
+        }
+    });
+}
+
+
